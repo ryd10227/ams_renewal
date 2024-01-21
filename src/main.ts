d87eac5645f1daf68807d9105219395b973a9a5f
seoyeon <ryd10227@gmail.com>
2024-01-22 03:10:37 +0900

    minor changes (types, etc...)

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import App from "./App.vue";
 import router from "@/router";
 import store from '@/store';
 
-import {
-  toastController, IonRadioGroup, IonRadio, IonSpinner, loadingController, useIonRouter, IonBackdrop, IonNote, IonItemSliding, IonItemOptions, IonItemOption, IonItemGroup, IonItemDivider, IonLabel, IonTextarea, IonInput, alertController, IonicVue, IonSelect,
-  IonSelectOption, IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter, IonItem, IonList, IonText, IonCheckbox, IonCard, IonCardTitle, IonCardHeader, IonCardContent
-} from '@ionic/vue';
+import { IonicVue, IonContent, IonHeader, IonPage, IonFooter, toastController, } from "@ionic/vue";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -36,43 +33,10 @@ const app = createApp(App)
 
 //registration global Components;
 app
-  .component("IonContent", IonContent)
   .component("IonPage", IonPage)
   .component("IonHeader", IonHeader)
-  .component("IonToolbar", IonToolbar)
-  .component("IonTitle", IonTitle)
+  .component("IonContent", IonContent)
   .component("IonFooter", IonFooter)
-  .component("IonBackButton", IonBackButton)
-  .component("IonItem", IonItem)
-  .component("IonList", IonList)
-  .component("IonText", IonText)
-  .component("IonSelect", IonSelect)
-  .component("IonSelectOption", IonSelectOption)
-  .component("IonButton", IonButton)
-  .component("IonButtons", IonButtons)
-  .component("alertController", alertController)
-  .component("IonCheckbox", IonCheckbox)
-  .component("IonInput", IonInput)
-  .component("IonTextarea", IonTextarea)
-  .component("IonLabel", IonLabel)
-  .component("IonItemDivider", IonItemDivider)
-  .component("IonItemGroup", IonItemGroup)
-  .component("IonItemOption", IonItemOption)
-  .component("IonItemOptions", IonItemOptions)
-  .component("IonItemSliding", IonItemSliding)
-  .component("IonNote", IonNote)
-  .component("IonBackdrop", IonBackdrop)
-  .component("useIonRouter", useIonRouter)
-  .component("loadingController ", loadingController)
-  .component("IonSpinner  ", IonSpinner)
-  .component("IonRadio ", IonRadio)
-  .component("IonRadioGroup ", IonRadioGroup)
-  .component("IonSelect ", IonSelect)
-  .component("IonCard", IonCard)
-  .component("IonCardTitle", IonCardTitle)
-  .component("IonCardHeader", IonCardHeader)
-  .component("IonCardContent", IonCardContent)
-  .component("IonSelectOption ", IonSelectOption)
   .component("toastController ", toastController);
 
 router.isReady().then(() => {
